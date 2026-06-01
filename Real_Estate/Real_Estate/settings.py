c6a98c1f96139c916e097328460204deab640768
kesava-projects <kesavaharshavardhan@gmail.com>
2026-06-02 00:38:01 +0530

    OAuth2.0

--- Real_Estate/Real_Estate/settings.py
+++ Real_Estate/Real_Estate/settings.py
@@ -184,8 +184,8 @@ SOCIALACCOUNT_PROVIDERS = {
     'google': {
 
         'APP': {
-            'client_id': os.getenv('GOOGLE_CLIENT_ID', 'GOOGLE_CLIENT_ID_REDACTED'),
-            'secret': os.getenv('GOOGLE_SECRET', 'GOOGLE_SECRET_REDACTED'),
+            'client_id': os.getenv('GOOGLE_CLIENT_ID'),
+            'secret': os.getenv('GOOGLE_SECRET'),
             'key': ''
         }
     }
